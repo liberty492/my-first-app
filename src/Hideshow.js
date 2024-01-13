eef7c15e2b7f4cc56b9a919d525a4763bc21486c
liberty492 <80382232+liberty492@users.noreply.github.com>
2024-01-13 07:00:46 +0530

    hide and show

---
+++ src/Hideshow.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './App.css';
+
+function Hideshow() {
+    const [showData, setShowData] = useState(false);
+
+    const hadleclick = () => {
+        setShowData(!showData);
+    }
+    return (
+        <div className="App">
+            <button onClick={hadleclick}>{showData ? 'hide' : 'show'}</button>
+            {/* {showData && <div>lorem reger jukel </div>} */}
+            {
+                showData ? (<div>lorem reger jukel </div>) : (<h3>data is not available</h3>)
+            }
+        </div>
+    );
+}
+
+export default Hideshow;
