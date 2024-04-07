8b83cf9e7c85c9a3d962305fa50501127c90561b
liberty492 <80382232+liberty492@users.noreply.github.com>
2024-04-07 16:03:14 +0530

    regular expression hooks

---
+++ src/Regularexpressions.js
@@ -0,0 +1,49 @@
+import react, { useState } from 'react'
+import { Card, CardTitle } from 'react-bootstrap';
+
+function Regularexpression() {
+
+    const [username, setUsername] = useState("");
+    const [error, setError] = useState("");
+
+
+    const handlechange = (e) => {
+        let data = e.target.value;
+        setUsername(data);
+    }
+
+    const validationError = () => {
+        if (username === "" || username === null) {
+            setError("username should not be empty");
+        }
+        else if (username.length < 4) {
+            setError(`username must be atleast 4 characters`);
+        }
+        else {
+            setError(<p style={{ color: "green",fontWeight:"bold"}}>good name..</p>);
+        }
+    }
+
+
+
+    return (
+        <div class="container">
+            <div class="row align-items-center vh-100">
+                <div class="col-6 mx-auto">
+                    <div class="card shadow border">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <CardTitle>username validation</CardTitle>
+                        </div>
+                        <div>
+                            <input type='text' id='username' name='username' value={username} onChange={handlechange} />&nbsp;&nbsp;
+                            <span style={{ color: "red" }}>{error}</span><br /><br />
+                            <button onClick={validationError}>vallidation</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default Regularexpression;
